6fdea42539999b6de735abccb16049badf077b6a
mbacelofernandez <mbacelofernandez@victoria12.danielcastelao.org>
2017-12-12 14:06:20 +0100

    Inicio proyecto

---
+++ src/boletin15/Boletin15.java
@@ -0,0 +1,12 @@
+package boletin15;
+
+public class Boletin15 {
+
+    public static void main(String[] args) {
+
+        NotaFinal alumno1 = new NotaFinal();
+
+        alumno1.calcularNota();
+    }
+
+}
